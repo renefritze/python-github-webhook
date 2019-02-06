b0b4773c5fc2e60bb2d34060164b2d825c6979f9
René Fritze <rene.fritze@wwu.de>
2019-02-06 10:42:47 +0100

    prefix package for disambiguation

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="github-webhook",
+setup(name="sanic-github-webhook",
       version="1.0.2",
       description="Very simple, but powerful, microframework for writing Github webhooks in Python",
       url="https://github.com/bloomberg/python-github-webhook",
       author="Alex Chamberlain, Fred Phillips, Daniel Kiss, Daniel Beer",
       author_email="achamberlai9@bloomberg.net, fphillips7@bloomberg.net, dkiss1@bloomberg.net, dbeer1@bloomberg.net",
       license='Apache 2.0',
-      packages=["github_webhook"],
-      install_requires=['flask', 'six'],
+      packages=["sanic_github_webhook"],
+      install_requires=['sanic', 'six'],
       tests_require=['mock', 'nose'],
 
       classifiers=[
